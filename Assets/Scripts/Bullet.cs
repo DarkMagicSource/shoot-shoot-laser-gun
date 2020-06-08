ba06a83f66a0cda1e959d30dc537381921ff2925
Caitlyn <katie.rose2604@gmail.com>
2020-06-08 20:41:14 +0930

    Tweak scipts

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,8 +5,6 @@ public class Bullet : MonoBehaviour
 {
   public float speed = 20f;
   public Rigidbody2D rb;
-
-  public TilemapController tileCtl;
   // public Tilemap tilemap;
   // public Vector3 tilePos;
 
@@ -22,7 +20,6 @@ public class Bullet : MonoBehaviour
     // Debug.Log(collision.name);
     if (collision.name == "Tilemap_Boxes")
     {
-      tileCtl.Collide();
       // code v1
       // // Debug.Log("Collision with non-player");
       // Debug.Log(collision.name);
