5839a147dcc052d85c1d38cf58839308bc047d3c
Caitlyn <katie.rose2604@gmail.com>
2020-06-10 11:29:56 +0930

    Added limited bullets system

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,14 +6,23 @@ public class Weapon : MonoBehaviour
   public Transform firePoint; // Position to fire bullet from
   [Tooltip("Reference to bullet prefab")]
   public GameObject bulletPrefab; // Bullet object to summon
+  public int mag = 3;
 
   void Update()
   {
     if (Input.GetKeyDown(KeyCode.E)) // Fire when player presses E
     {
-      // Shooting logic
-      // Debug.Log("Fire!");
-      Instantiate(bulletPrefab, firePoint.position, firePoint.rotation); // Create bullet object from prefab
+      if (mag >= 1)
+      {
+        // Shooting logic
+        // Debug.Log("Fire!");
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation); // Create bullet object from prefab
+        mag = mag - 1;
+      }
+      else
+      {
+        Debug.Log("get more ammo loser");
+      }
     }
   }
 }
