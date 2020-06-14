93393b3a85fa1552be37ef355afc8d282df27bed
Caitlyn <katie.rose2604@gmail.com>
2020-06-15 09:19:55 +0930

    Improve bullet system

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,6 +6,8 @@ public class Bullet : MonoBehaviour
   [Tooltip("How fast the bullet travels")]
   public float speed = 10f;
   private Rigidbody2D rb;
+  [Tooltip("Impact effect prefab")]
+  public GameObject impactEffect;
 
   void Start()
   {
@@ -13,13 +15,16 @@ public class Bullet : MonoBehaviour
     rb.velocity = transform.right * speed; // Propels bullet forward
   }
 
-  void OnTriggerEnter2D(Collider2D collision)
+
+  void OnTriggerStay2D(Collider2D collision)
   {
     // Debug.Log("Inital Collision " + collision.name);
     if (collision.name == "Tilemap_Boxes") // Test if bullet is hitting boxes tilemap layer
     {
+      Instantiate(impactEffect, transform.position, transform.rotation);
       var map = collision.gameObject.GetComponent<Tilemap>(); // Get tilemap component from collided object
       var tilePos = map.WorldToCell(transform.position); // Get position of tile hit
+      Debug.Log("Tile hit: " + tilePos);
       map.SetTile(tilePos, null); // Remove said tile
       Destroy(gameObject); // Remove bullet
     }
