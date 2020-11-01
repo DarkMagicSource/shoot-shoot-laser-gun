e80483cf540eebc752b701b521fd9c5392538804
Caitlyn Rose Williams <cait.rose2604@gmail.com>
2020-11-02 09:59:51 +1030

    Add initial boss logic and debugging

---
+++ Assets/Scripts/BossController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Random = System.Random;
+
+public class BossController : MonoBehaviour
+{
+  Random random = new Random();
+
+  int timeRemaining = -1;
+
+  void Start()
+  {
+    Debug.Log("Script Start");
+  }
+
+  void Update()
+  {
+    if (timeRemaining == -1) timeRemaining = AttackCooldown();
+    Debug.Log("Time Remaining: " + timeRemaining);
+    if (timeRemaining < 1) Debug.Log("Attack type " + GetAttackId());
+    else timeRemaining--;
+  }
+
+  int GetAttackId()
+  {
+    int attackId = random.Next(0, 3);
+    return attackId;
+  }
+
+  int AttackCooldown()
+  {
+    int AttackCooldown = random.Next(1000, 3000);
+    return AttackCooldown;
+  }
+}
