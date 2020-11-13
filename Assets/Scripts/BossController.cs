8a5a51f9ce6dcac5f8259f5d1897d68b47dc0d2a
Caitlyn Rose Williams <cait.rose2604@gmail.com>
2020-11-13 14:05:46 +1030

    Improve boss logic with random attack cooldown

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -1,34 +1,72 @@
-﻿using UnityEngine;
-using Random = System.Random;
+﻿using System.Collections;
+using UnityEngine;
+using Random = System.Random; // Force all randoms to use System.Random
 
 public class BossController : MonoBehaviour
 {
-  Random random = new Random();
-
-  int timeRemaining = -1;
-
-  void Start()
-  {
-    Debug.Log("Script Start");
-  }
+  public GameObject Player; // Reference to player object
+  Random random = new Random(); // initialize random
+  bool currentlyAttacking = false;
 
   void Update()
   {
-    if (timeRemaining == -1) timeRemaining = AttackCooldown();
-    Debug.Log("Time Remaining: " + timeRemaining);
-    if (timeRemaining < 1) Debug.Log("Attack type " + GetAttackId());
-    else timeRemaining--;
+    Vector3 playerPos = Player.transform.position;
+    Vector3 bossPos = transform.position;
+    float distance = Vector3.Distance(playerPos, bossPos);
+
+    if (distance <= 5)
+    {
+      if (!currentlyAttacking)
+      {
+        StartCoroutine(CalculateAttack());
+      }
+    }
   }
 
   int GetAttackId()
   {
-    int attackId = random.Next(0, 3);
-    return attackId;
+    return random.Next(1, 4); // get random value to be used as attack ID
   }
 
-  int AttackCooldown()
+  int GetAttackCooldown()
   {
-    int AttackCooldown = random.Next(1000, 3000);
-    return AttackCooldown;
+    return random.Next(5, 9); // get random value to be used as cooldown between attacks
+  }
+
+  IEnumerator CalculateAttack()
+  {
+    int AttackId = GetAttackId();
+    int attackCooldown = GetAttackCooldown();
+    currentlyAttacking = true;
+
+    switch (AttackId)
+    {
+      case 1:
+        Attack1();
+        break;
+      case 2:
+        Attack2();
+        break;
+      case 3:
+        Attack3();
+        break;
+    }
+
+    Debug.Log("Attack Cooldown: " + attackCooldown + "s");
+    yield return new WaitForSeconds(attackCooldown);
+
+    currentlyAttacking = false;
+  }
+
+  void Attack1() {
+    Debug.Log("Attack 1");
+  }
+
+  void Attack2() {
+    Debug.Log("Attack 2");
+  }
+
+  void Attack3() {
+    Debug.Log("Attack 3");
   }
 }
